5a607fc49a3dde925947200432a2f233ec757c76
gaslight <97.Bernhard@gmail.com>
2022-12-30 18:02:58 +0100

    update gitignore

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD Application Using PHP OOPS PDO MySQL & FETCH API of ES6</title>
+    <title>CRUD App</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.bundle.min.js"></script>
 </head>
@@ -98,7 +98,8 @@
     <div class="row mt-4">
         <div class="col-lg-12 d-flex justify-content-between align-items-center">
             <div>
-                <h4 class="text-primary">All users in the database!</h4>
+                <h4 class="text-primary">user accounts listed in the db</h4>
+                <h6 class="test-primary">CRUD Application Using PHP OOPS PDO MySQL & FETCH API of ES6</h6>
             </div>
             <div>
                 <button class="btn btn-primary" type="button" data-toggle="modal" data-target="#addNewUserModal">Add New User</button>
